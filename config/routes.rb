eaa881c7bbd153c4a30c4b08c26f50cba2570acf
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-02-28 13:32:51 -0800

    Add Rooms table, model, controller and views. Routes also

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
   
   devise_for :users,
@@ -8,5 +9,15 @@ Rails.application.routes.draw do
 
 
   resources :users, only: [:show]
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
