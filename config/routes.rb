492a6bbc79e960620e4961b7eb69781efb947b01
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-02 12:23:27 -0800

    Add more functions on room show and add your trips page

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,14 @@ Rails.application.routes.draw do
       get 'photo_upload'
       get 'amenities'
       get 'location'
-
+      get 'preload'
+      get 'preview'
     end
     
     resources :photos, only: [:create, :destroy]
     resources :reservations, only: [:create]
   end
+
+  get '/your_trips' => 'reservations#your_trips'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
