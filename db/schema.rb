75d2da0b30695fce2dc68c5c0719f45537e043d1
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-03 19:46:03 -0800

    Delete reviews table and readded it to fix reviews error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_210302) do
+ActiveRecord::Schema.define(version: 2019_03_04_032444) do
 
   create_table "photos", force: :cascade do |t|
     t.integer "room_id"
