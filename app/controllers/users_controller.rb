329247f00d11a3c26fe2957685cc23635f29fd01
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-10 15:53:02 -0700

    Add #add_card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+  
   def show
     @user = User.find(params[:id])
     @rooms = @user.rooms
@@ -34,6 +36,41 @@ class UsersController < ApplicationController
   rescue Exception => e
     redirect_to edit_user_registration_path, alert: "#{e.message}"
   end
+
+#stripe 
+
+def payment
+end
+
+
+def add_card
+  if current_user.stripe_id.blank?
+    customer = Stripe::Customer.create(
+      email: current_user.email
+    )
+    current_user.stripe_id = customer.id
+    current_user.save
+  else
+    customer = Stripe::Customer.retrieve(current_user.stripe_id)
+  end
+
+  #add cc to stripe
+  month, year = params[:expiry].split(/ \/ /)
+  new_token = Stripe::Token.create(:card => {
+    :number => params[:number],
+    :exp_month => month,
+    :exp_year => year,
+    :cvv => params[:cvv]
+  })
+  customer.sources.create(source: new_token.id)
+
+  flash[:notice] = "Your Card has been saved."
+  redirect_to payment_method_path
+rescue Stripe::CardError => e  
+  flash[:alert] = e.message
+  redirect_to payment_method_path
+end
+
 
   private
 
