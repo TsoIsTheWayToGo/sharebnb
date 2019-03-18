fd70f3d456f5aacf72f581e7962b170862614109
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-17 17:49:37 -0700

    Add #payout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,38 +39,38 @@ class UsersController < ApplicationController
 
 #stripe 
 
-def payment
-end
-
-
-def add_card
- 
-  if current_user.stripe_id.blank?
-    customer = Stripe::Customer.create(
-      email: current_user.email
-    )
-    current_user.stripe_id = customer.id
-    current_user.save
-  else
-    customer = Stripe::Customer.retrieve(current_user.stripe_id)
+  def payment
   end
 
-  #add cc to stripe
-  month, year = params[:expiry].split(/ \/ /)
-  new_token = Stripe::Token.create(:card => {
-    :number => params[:number],
-    :exp_month => month,
-    :exp_year => year,
-    :cvc => params[:cvv]
-  })
-  customer.sources.create(source: new_token.id)
+  def payout
+    if !current_user.merchant_id.blank?
+      account = Stripe::Account.retrieve(current_user.merchant_id)
+      @login_link = account.login_links.create()
+    end
+  end
 
-  flash[:notice] = "Your Card has been saved."
-  redirect_to payment_method_path
-rescue Stripe::CardError => e  
-  flash[:alert] = e.message
-  redirect_to payment_method_path
-end
+  def add_card
+    if current_user.stripe_id.blank?
+      customer = Stripe::Customer.create(
+        email: current_user.email
+      )
+      current_user.stripe_id = customer.id
+      current_user.save
+
+      # Add Credit Card to Stripe
+      customer.sources.create(source: params[:stripeToken])
+    else
+      customer = Stripe::Customer.retrieve(current_user.stripe_id)
+      customer.source = params[:stripeToken]
+      customer.save
+    end
+
+    flash[:notice] = "Your card is saved."
+    redirect_to payment_method_path
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to payment_method_path
+  end
 
 
   private
