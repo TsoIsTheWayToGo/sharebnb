7ec528ac4ddf12b8cdf5b283ff70ad5eef63eb16
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-19 16:02:41 -0700

    Add validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,8 @@
 class Reservation < ApplicationRecord
   enum status: {Waiting: 0, Approved: 1, Declined: 2}
 
+  after_create_commit :create_notification
+
   belongs_to :user
   belongs_to :room
 
@@ -10,9 +12,17 @@ class Reservation < ApplicationRecord
     .order(updated_at: :asc)
   }
 
-  
+  private
+
+    def create_notification
+      type = self.room.Instant? ? "New Booking" : "New Request"
+      guest = User.find(self.user_id)
+
+      Notification.create(content: "#{type} from #{guest.fullname}", user_id: self.room.user_id)
+    end
+end
+
 # def approved_reservations
 #   self.where(status: 1)
 # end
 
-end
