9250719fffa87e2457f8726147f0fb249c8953a0
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-18 18:17:02 -0700

    Add #index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,12 @@ class MessagesController < ApplicationController
   before_action :set_conversation
 
   def index
-
+    if current_user == @conversation.sender || current_user == @conversation.recipient
+      @other = current_user == @conversation.sender ? @conversation.recipient : @conversation.sender
+      @messages = @conversation.messages.order("created_at DESC")
+    else
+      redirect_to conversations_path, alert: "You don't have permission to view this."
+    end
   end
 
   def create
@@ -11,6 +16,10 @@ class MessagesController < ApplicationController
   end
 
   private
+
+    def set_conversation
+      @conversation = Conversation.find(params[:conversation_id])
+    end
 
     def message_params
       params.require(:message).permit(:context, :user_id)
