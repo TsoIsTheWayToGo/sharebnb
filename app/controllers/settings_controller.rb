16d511486a16bbd709345eafba150835add546eb
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-18 17:28:12 -0700

    Add #update

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,4 +3,14 @@ class SettingsController < ApplicationController
     @setting = User.find(current_user.id).setting
   end
 
+  def update
+    @setting = User.find(current_user.id).setting
+    if @setting.update(setting_params)
+      flash[:notice] = "Saved..."
+    else
+      flash[:alert] = "Cannot save..."
+    end
+    render 'edit'
+  end
+
 end
