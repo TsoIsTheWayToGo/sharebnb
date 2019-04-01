cdcbf1c0d9dde8f86d4a9b2585978d2a9b7d24ab
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-31 17:52:41 -0700

    Add production configuarations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,11 +33,21 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-
+# config.action_mailer.default_url_options = { :host => 'yourapp.heroku.com' 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_mailer.default_url_options = { :host => 'sharebnbb.heroku.com' }
 
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      enable_starttls_auto: true,
+      authentication: 'plain',
+      user_name: Rails.application.credentials.gmail[:username],
+      password: Rails.application.credentials.gmail[:password]
+  }
+  
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
   config.require_master_key = true
